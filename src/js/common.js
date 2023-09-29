6e3c7a368933338ee2a407f04ac6100748361bb1
Your Name <yang.lin.99@bk.ru>
2023-09-29 21:37:57 +0300

    end of firste section

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  (function accordions() {
+    let buttons = document.querySelectorAll(".bonus-content-info__btn");
+    let accordions = document.querySelectorAll(".bonus-accordion");
+    buttons.forEach((button, index) => {
+      let icon = button.querySelector(".bonus-content-info__img-check");
+      button.addEventListener("click", () => {
+        icon.classList.toggle("active");
+        accordions[index].classList.toggle("active");
+      });
+    });
+  })();
+});
