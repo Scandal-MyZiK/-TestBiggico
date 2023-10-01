71d8fa2ab2de80af14aad2712ba760f41067432e
Your Name <yang.lin.99@bk.ru>
2023-10-01 16:52:31 +0300

    End of pari-match section

--- webpack.config.js
+++ webpack.config.js
@@ -1,110 +1,125 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const fs = require("fs");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
-const devMode = mode === 'development';
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+const pages = fs
+  .readdirSync(path.resolve(__dirname, "src"))
+  .filter((fileName) => fileName.endsWith(".html"));
 
 module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-        port:3001,
-        open: true,
-        hot: true,
-    },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        filename: '[name].[contenthash].js',
-        assetModuleFilename: 'assets/[name][ext]'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
-        }),
-        new MiniCssExtractPlugin ({
-            filename: '[name].[contenthash].css',
-        }),
-        // new CopyWebpackPlugin({
-		// 	patterns: [
-		// 		{from: path.resolve(__dirname, 'src/static'), to: ''}
-		// 	]
-		// })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3001,
+    open: true,
+    hot: true,
+  },
+  entry: ["@babel/polyfill", path.resolve(__dirname, "src", "index.js")],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  plugins: [
+    // ...pages.map(
+    //   (page) =>
+    //     new HtmlWebpackPlugin({
+    //       template: `src/${page}`,
+    //       filename: `./${page}`,
+    //     })
+    // ),
+    new HtmlWebpackPlugin({
+      template: `src/pari-match.html`,
+      filename: `./pari-match.html`,
+    }),
+    new HtmlWebpackPlugin({
+      template: `src/index.html`,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    // new CopyWebpackPlugin({
+    // 	patterns: [
+    // 		{from: path.resolve(__dirname, 'src/static'), to: ''}
+    // 	]
+    // })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("postcss-preset-env")],
+              },
             },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [require('postcss-preset-env')],
-                            },
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.woff2?$/i,
-                type: 'asset/resource',
-                generator: {
-                  filename: 'fonts/[name].[ext]'
-                }
-            },
-            {
-                test: /\.(jpe?g|png|webp|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                              progressive: true,
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                              enabled: false,
-                            },
-                            pngquant: {
-                              quality: [0.65, 0.90],
-                              speed: 4
-                            },
-                            gifsicle: {
-                              interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                              quality: 75
-                            },
-                        }
-                    }
-                ],
-                type: 'asset/resource',
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-              }
+          },
+          "sass-loader",
         ],
-    },
+      },
+      {
+        test: /\.woff2?$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
